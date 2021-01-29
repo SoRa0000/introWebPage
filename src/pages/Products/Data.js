22c2413af4cb0e8aa14d705aa7000c0ea62d4652
SoRa0000 <d.bayasaa1230@yahoo.com>
2021-01-29 10:39:58 +0800

    .

--- src/pages/Products/Data.js
+++ src/pages/Products/Data.js
@@ -1,51 +1,67 @@
-  export const homeObjTwo = {
-    primary: true,
-    lightBg: false,
-    lightTopLine: true,
-    lightText: true,
-    lightTextDesc: true,
-    topLine: 'Instant Setup',
-    headline: 'Extremely quick onboarding process',
-    description:
-      "Once you've joined, our team of specialist will reach out to you and get you set up in minutes.",
-    buttonLabel: 'Learn More',
-    imgStart: '',
-    img: '/images/svg-2.svg',
-    alt: 'Vault',
-    start: ''
-  };
+export const homeObjOne = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Тоолуур',
+  headLine: 'Шилдэг тоолууруудыг зөвхөн манайхаас',
+  description:'Орчин үеийн шинэ, шинэлэг, шилдэг тоолуурыг зөвхөн манай байгууллага оруулж ирж байна',
+  buttonLabel: 'Эхлэх',
+  imgStart: '',
+  img: '/images/svg-1.svg',
+  alt: 'Credit Card',
+  start: ''
+};
 
-  export const homeObjThree = {
-    primary: false,
-    lightBg: true,
-    lightTopLine: false,
-    lightText: false,
-    lightTextDesc: false,
-    topLine: 'Sarah Jeni',
-    headline:
-      'Ultra helped me increase my revenue by over 3X in less than 3 months!',
-    description:
-      "Their team is wonderful! I can't believe I didn't start working with them earlier.",
-    buttonLabel: 'View Case Study',
-    imgStart: 'start',
-    img: '/images/profile.jpg',
-    alt: 'Vault',
-    start: 'true'
-  };
+export const homeObjTwo = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Түргэн суурилуулалт',
+  headLine: 'Найдвартай хурдан үйлчилгээ',
+  description:
+    "Цаг хугацааны алдагдалгүй хурдан шуурхай, найдвартай суурилуулалтын үйлчилгээг үзүүлэх болно",
+  buttonLabel: 'Цааш үзэх',
+  imgStart: '',
+  img: '/images/svg-2.svg',
+  alt: 'Vault',
+  start: ''
+};
 
-  export const homeObjFour = {
-    primary: true,
-    lightBg: true,
-    lightTopLine: false,
-    lightText: false,
-    lightTextDesc: false,
-    topLine: 'Secure Database',
-    headline: 'All your data is stored on our secure server',
-    description:
-      'You will never have to worry about your information getting leaked. Our team of security experts will ensure your records are kept safe.',
-    buttonLabel: 'Sign Up Now',
-    imgStart: 'start',
-    img: '/images/svg-3.svg',
-    alt: 'Vault',
-    start: 'true'
-  };
+export const homeObjThree = {
+  primary: false,
+  lightBg: true,
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Тогны тоолуур',
+  headLine:
+    'Хятад улсад үйлдвэрлэсэн цоо шинэ тоолуурууд',
+  description:
+    "Таны хүссэн тоо хэмжээгээр, шуурхай мөн найдвартайгаар оруулж ирэх болно",
+  buttonLabel: 'Захиалах',
+  imgStart: 'start',
+  img: '/images/profile.jpg',
+  alt: 'Vault',
+  start: 'true'
+};
+
+export const homeObjFour = {
+  primary: true,
+  lightBg: true,
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Аюулгүй ажиллагаа',
+  headLine: 'Хамгийн найдвартай, аюулгүй бүтээгдэхүүн',
+  description:
+    'Бид танд зөвхөн хамгийн найдвартай аюулгүй үйл ажиллагаатай бараа бүтээгдэхүүн, төхөөрөмжүүдийг санал болнох болно.',
+  buttonLabel: 'Цааш үзэх',
+  imgStart: 'start',
+  img: '/images/svg-3.svg',
+  alt: 'Vault',
+  start: 'true'
+};
